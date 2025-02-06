e8142661bb4c9be86b95d5e6c8d280d03d850559
Jeff <doserjef@msu.edu>
2025-02-06 05:40:32 -0500

    porting over vignettes and fix bug in writeFIA

--- R/writeFIA.R
+++ R/writeFIA.R
@@ -24,28 +24,52 @@ writeFIA <- function(db,
   # Method to chunk up the database into states before writing it out
   if (byState){
 
-    db$PLOT <- db$PLOT %>%
-      select(-c(any_of('STATEAB'))) %>%
-      left_join(intData$stateNames, by = 'STATECD')
+    if (!is.null(db$PLOT)) {
+      db$PLOT <- db$PLOT %>%
+        dplyr::select(-c(any_of('STATEAB'))) %>%
+        dplyr::left_join(intData$stateNames, by = 'STATECD')
 
-    # Unique state abbreviations
-    states <- unique(db$PLOT$STATEAB)
+      # Unique state abbreviations
+      states <- unique(db$PLOT$STATEAB)
 
-    # Chunk up plot
-    pltList <- split(db$PLOT, as.factor(db$PLOT$STATEAB))
+      # Chunk up plot
+      pltList <- split(db$PLOT, as.factor(db$PLOT$STATEAB))
+
+    } else if (!is.null(db$COND)) {
+      db$COND <- db$COND %>%
+        dplyr::select(-c(any_of('STATEAB'))) %>%
+        dplyr::left_join(intData$stateNames, by = 'STATECD')
+
+      # Unique state abbreviations
+      states <- unique(db$COND$STATEAB)
+
+      # Chunk up plot
+      pltList <- split(db$COND, as.factor(db$COND$STATEAB))
+
+    } else {
+      stop("writing FIADB tables with byState = TRUE requires either the PLOT or COND tables to be loaded.")
+    }
 
     # Loop over states, do the writing
     for (s in 1:length(states)){
       db_clip <- db
-      # Overwriting plot with shortened table
-      db_clip$PLOT <- pltList[[s]]
+      if (!is.null(db$PLOT)) {
+        # Overwriting plot with shortened table
+        db_clip$PLOT <- pltList[[s]]
+      } else {
+        db_clip$COND <- pltList[[s]]
+      }
       # Subsetting the remaining database based
       # on plot table
-      db_clip <- clipFIA(db_clip, mostRecent = FALSE)
+      # db_clip <- clipFIA(db_clip, mostRecent = FALSE)
 
       # Write it all out
       tableNames <- names(db_clip)[names(db_clip) != 'mostRecent']
-      tableNames <- paste(unique(db_clip$PLOT$STATEAB), tableNames, sep = '_')
+      if (!is.null(db$PLOT)) {
+        tableNames <- paste(unique(db_clip$PLOT$STATEAB), tableNames, sep = '_')
+      } else {
+        tableNames <- paste(unique(db_clip$COND$STATEAB), tableNames, sep = '_')
+      }
       for (i in 1:length(tableNames)){
         if (is.data.frame(db_clip[[i]])){
           fwrite(x = db_clip[[i]], file = paste0(dir, tableNames[i], '.csv'), showProgress = FALSE, nThread = nCores)
